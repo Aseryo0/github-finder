0cdc4623fa887e506a085f4be521b8c463389c70
Asero0 <andrefelipeil@hotmail.com>
2023-04-15 09:52:05 -0300

    fim do projeto

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
+import { Outlet } from "react-router-dom"
+import classes from "./App.module.css"
+
 function App() {
 
   return (
-    <div>Olá</div>
+
+    <div className={classes.app}>
+      <h1>Github Finder!</h1>
+      <Outlet />
+    </div>
 
   )
 }
---
+++ src/routes/Home.tsx
@@ -0,0 +1,42 @@
+import Search from "../components/Search"
+import { userProps } from "../types/User"
+import { useState } from "react"
+import User from "../components/User"
+import Error from "../components/Error"
+
+const Home = () => {
+    const [user, setUser] = useState<userProps | null>(null)
+    const [error, setError] = useState(false)
+
+    const loadUser = async (userName: string) => {
+        setUser(null)
+        setError(false)
+        const res = await fetch(`https://api.github.com/users/${userName}`)
+
+        const data = await res.json()
+
+        if (res.status === 404) {
+            setError(true)
+            return
+        }
+
+        const { avatar_url, login, location, followers, following } = data
+
+        const userData = {
+            avatar_url,
+            login,
+            location,
+            followers,
+            following,
+        }
+        setUser(userData)
+    }
+    return (
+        <div>
+            <Search loadUser={loadUser} />
+            {user && <User {...user} />}
+            {error && <Error />}
+        </div>)
+}
+
+export default Home
